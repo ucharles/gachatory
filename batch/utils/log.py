6d176d1725eb0a87e3bcaaf1303e4afbe14b3e33
JungHyun Yoo <jhyoo728@gmail.com>
2023-08-31 02:02:53 +0900

    Crontab 사용을 위해 절대 경로 추가
    - 상대 경로를 기준으로 수행되던 로직을 절대 경로로 수정

--- batch/utils/log.py
+++ batch/utils/log.py
@@ -1,19 +1,34 @@
 import os
 from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+absolute_path_bandai = os.getenv("ABSOLUTE_PATH_BANDAI")
 
 
-def log(maker, page, url, status_code, msg=""):
+def brand_path_selector(brand):
+    if brand == "bandai":
+        return absolute_path_bandai
+    else:
+        return ""
+
+
+def log(brand, page, url, status_code, msg="", path=""):
     now = datetime.now()
     nowDate = now.strftime("%Y%m%d")
     nowDatetime = now.strftime("%Y%m%d-%H%M%S")
 
     # log 폴더가 없을 경우
-    if not os.path.isdir("log"):
+    if not os.path.isdir(brand_path_selector(brand) + "log"):
         os.makedirs("log")
 
-    f = open("log/" + maker + "_" + nowDate + ".txt", "a", encoding="utf-8")
+    f = open(
+        brand_path_selector(brand) + "log/" + brand + "_" + nowDate + ".txt",
+        "a",
+        encoding="utf-8",
+    )
     f.write("date:" + nowDatetime + ",")
-    f.write("maker:" + maker + ",")
+    f.write("brand:" + brand + ",")
     f.write("page:" + str(page) + ",")
     f.write("status_code:" + str(status_code) + ",")
     f.write("url:'" + url + "'")
