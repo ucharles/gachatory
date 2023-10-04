37367047b0c04921efd484c9d88a72821d648c20
JungHyun Yoo <jhyoo728@gmail.com>
2023-10-04 22:34:57 +0900

    <TenstackProvider>에 전역 cacheTime, staleTime 추가

--- next-app/src/app/[lng]/components/Providers/TenstackProvider.tsx
+++ next-app/src/app/[lng]/components/Providers/TenstackProvider.tsx
@@ -3,6 +3,7 @@
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
+import { cacheTimeEnum } from "@/lib/enums";
 
 export default function TenstackProvider({
   children,
@@ -16,9 +17,11 @@ export default function TenstackProvider({
         queries: {
           refetchOnWindowFocus: false,
           retry: false,
+          cacheTime: cacheTimeEnum.ONE_MINUTE * 1000,
+          staleTime: cacheTimeEnum.FIVE_MINUTES * 1000,
         },
       },
-    })
+    }),
   );
   return (
     <QueryClientProvider client={queryClient}>
