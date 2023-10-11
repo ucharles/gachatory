55f85c6e3ee2fa294c75a1ae85eb2a3f5bad1734
JungHyun Yoo <jhyoo728@gmail.com>
2023-10-12 02:37:06 +0900

    date_convert_to_ISO 코드 수정 및 테스트 추가

--- batch/utils/date_convert_to_ISO.py
+++ batch/utils/date_convert_to_ISO.py
@@ -1,29 +1,31 @@
 from typing import List
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def date_convert_to_iso(dates: str):
+    """
+    Convert a date string to ISO format.
+
+    Args:
+        dates (str): The date string to convert.
+
+    Returns:
+        str: The date string in ISO format.
+    """
     date_str = dates.strip()
-    if "上旬" in date_str:
-        parsed_date = datetime.strptime(
-            f"{date_str[:4]}-{date_str[5:7]}-01", "%Y-%m-%d"
-        )
-    elif "中旬" in date_str:
-        parsed_date = datetime.strptime(
-            f"{date_str[:4]}-{date_str[5:7]}-15", "%Y-%m-%d"
-        )
-    elif "下旬" in date_str:
-        year = int(date_str[:4])
-        month = int(date_str[5:7])
-        last_day = (datetime(year, month + 1, 1) - timedelta(days=1)).day
-        parsed_date = datetime.strptime(
-            f"{date_str[:4]}-{date_str[5:7]}-{last_day}", "%Y-%m-%d"
-        )
+    year = int(date_str[:4])
+    month = int(date_str[5:7])
+
+    mapping = {"上旬": "01", "中旬": "15", "下旬": "last_day"}
+
+    if date_str[-2:] in mapping:
+        if mapping[date_str[-2:]] == "last_day":
+            last_day = (datetime(year, month + 1, 1) - timedelta(days=1)).day
+            parsed_date = datetime(year, month, last_day)
+        else:
+            parsed_date = datetime(year, month, int(mapping[date_str[-2:]]))
     else:
-        year = int(date_str[:4])
-        month = int(date_str[5:7])
         last_day = (datetime(year, month + 1, 1) - timedelta(days=1)).day
-        parsed_date = datetime.strptime(
-            f"{date_str[:4]}-{date_str[5:7]}-{last_day}", "%Y-%m-%d"
-        )
+        parsed_date = datetime(year, month, last_day)
+
     return parsed_date.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
