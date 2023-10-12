cf1110c11560e8b08c9374f9ee5bbe09e75e129f
JungHyun Yoo <jhyoo728@gmail.com>
2023-10-12 23:30:26 +0900

    date_convert_to_ISO.py에 예외처리 추가

--- batch/utils/date_convert_to_ISO.py
+++ batch/utils/date_convert_to_ISO.py
@@ -12,20 +12,23 @@ def date_convert_to_iso(dates: str):
     Returns:
         str: The date string in ISO format.
     """
-    date_str = dates.strip()
-    year = int(date_str[:4])
-    month = int(date_str[5:7])
+    try:
+        date_str = dates.strip()
+        year = int(date_str[:4])
+        month = int(date_str[5:7])
 
-    mapping = {"上旬": "01", "中旬": "15", "下旬": "last_day"}
+        mapping = {"上旬": "01", "中旬": "15", "下旬": "last_day"}
 
-    if date_str[-2:] in mapping:
-        if mapping[date_str[-2:]] == "last_day":
+        if date_str[-2:] in mapping:
+            if mapping[date_str[-2:]] == "last_day":
+                last_day = (datetime(year, month + 1, 1) - timedelta(days=1)).day
+                parsed_date = datetime(year, month, last_day)
+            else:
+                parsed_date = datetime(year, month, int(mapping[date_str[-2:]]))
+        else:
             last_day = (datetime(year, month + 1, 1) - timedelta(days=1)).day
             parsed_date = datetime(year, month, last_day)
-        else:
-            parsed_date = datetime(year, month, int(mapping[date_str[-2:]]))
-    else:
-        last_day = (datetime(year, month + 1, 1) - timedelta(days=1)).day
-        parsed_date = datetime(year, month, last_day)
 
-    return parsed_date.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        return parsed_date.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    except:
+        return "Invalid date string"
