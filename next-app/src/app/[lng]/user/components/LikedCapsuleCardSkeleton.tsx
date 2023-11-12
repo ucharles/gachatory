04f9452de3df1820f6d22a3f1d4f1d1a0dadb45f
JungHyun Yoo <jhyoo728@gmail.com>
2023-11-12 21:42:40 +0900

    dashboard의 최근 좋아요 목록을 CSR로 변경
    - SSR -> CSR로 변경함으로써 좋아요 목록을 즉시 불러오도록 함
    (매번 data를 fetch하지 않도록 추후 ReactQuery를 사용해야 함)
    - CSR로 변경함에 따른 스켈레톤 UI 추가

--- next-app/src/app/[lng]/user/components/LikedCapsuleCardSkeleton.tsx
+++ next-app/src/app/[lng]/user/components/LikedCapsuleCardSkeleton.tsx
@@ -1,16 +1,14 @@
-function LikedCapsuleCardSkeleton({ isFirst }: { isFirst?: boolean }) {
+function LikedCapsuleCardSkeleton() {
   const items = Array.from({ length: 4 });
   return (
-    <ul
-      className={`grid gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 fold:grid-cols-2 3xs:grid-cols-2 2xs:grid-cols-2 xs:grid-cols-2`}
-    >
+    <>
       {items.map((_, index) => (
         <li key={index} className="animate-pulse">
           <div className="mb-4 flex aspect-square items-center justify-center overflow-hidden rounded-md bg-gray-100"></div>
-          <div className="w-full rounded-md bg-gray-100 py-14"></div>
+          <div className="w-full rounded-md bg-gray-100 py-16"></div>
         </li>
       ))}
-    </ul>
+    </>
   );
 }
 
--- next-app/src/app/[lng]/user/components/RecentLikeList.tsx
+++ next-app/src/app/[lng]/user/components/RecentLikeList.tsx
@@ -1,20 +1,27 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import LikedCapsuleCardSkeleton from "./LikedCapsuleCardSkeleton";
 import LikedCapsuleCard from "./LikedCapsuleCard";
 import { fetchLikedData } from "@/lib/fetch-data";
-import { cookies } from "next/headers";
+import LikedCapsuleCardSkeleton from "./LikedCapsuleCardSkeleton";
 
-export default async function RecentLikeList({
+export default function RecentLikeList({
   lng,
   searchParams,
 }: {
   lng: string;
   searchParams: Record<string, string>;
 }) {
+  const [data, setData] = useState<any>(null);
   const limit = searchParams.limit || "4";
-  const cookie = cookies();
 
-  const data: any = await fetchLikedData(lng, limit, cookie);
+  useEffect(() => {
+    fetchLikedData(lng, limit).then((res) => {
+      setData(res);
+    });
+  }, [lng, limit]);
+
   return (
     <article className="space-y-4">
       <div className="flex items-end justify-between">
@@ -24,16 +31,20 @@ export default async function RecentLikeList({
         </span>
       </div>
       <ul className="grid gap-x-6 gap-y-6 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 fold:grid-cols-2 3xs:grid-cols-2 2xs:grid-cols-2 xs:grid-cols-2">
-        {data?.likes?.map((like: any) => (
-          <LikedCapsuleCard
-            id={like.capsuleId._id}
-            name={like.capsuleId.name}
-            date={like.capsuleId.date}
-            img={like.capsuleId.display_img}
-            tags={like.capsuleId.tagId}
-            lng={lng}
-          />
-        ))}
+        {data ? (
+          data?.likes?.map((like: any) => (
+            <LikedCapsuleCard
+              id={like.capsuleId._id}
+              name={like.capsuleId.name}
+              date={like.capsuleId.date}
+              img={like.capsuleId.display_img}
+              tags={like.capsuleId.tagId}
+              lng={lng}
+            />
+          ))
+        ) : (
+          <LikedCapsuleCardSkeleton />
+        )}
       </ul>
     </article>
   );
