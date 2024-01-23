3beefc4f0821146b03fcac1a3a2ea2ac8d6cde64
JungHyun Yoo <jhyoo728@gmail.com>
2024-01-24 00:24:47 +0900

    Update translate_utils.ts

--- batch/utils/translate_utils.ts
+++ batch/utils/translate_utils.ts
@@ -46,11 +46,18 @@ export function bracketMatcher(translated: string, original?: string) {
   const translatedLeftBracket = translated.match(leftBracketRegex);
   const translatedRightBracket = translated.match(rightBracketRegex);
   const translatedFirstCharInRegex = translated[0].match(leftBracketRegex);
+  const translatedLastCharInRegex =
+    translated[translated.length - 1].match(rightBracketRegex);
+
+  // DeepL API 사용 시
+  // 문자열 처음과 끝에 괄호가 있으면 제거되는 문제가 있어
+  // 이를 해결하기 위한 함수 작성
 
   let leftBracket = "";
+  let rightBracket = "";
 
   if (
-    translatedRightBracket?.length !== translatedLeftBracket?.length &&
+    translatedRightBracket?.length > translatedLeftBracket?.length &&
     !translatedFirstCharInRegex
   ) {
     const rightBracket = translatedRightBracket[0];
@@ -67,9 +74,27 @@ export function bracketMatcher(translated: string, original?: string) {
       default:
         break;
     }
+  } else if (
+    translatedRightBracket?.length < translatedLeftBracket?.length &&
+    !translatedLastCharInRegex
+  ) {
+    const leftBracket = translatedLeftBracket[0];
+    switch (leftBracket) {
+      case "(":
+        rightBracket = ")";
+        break;
+      case "[":
+        rightBracket = "]";
+        break;
+      case "【":
+        rightBracket = "】";
+        break;
+      default:
+        break;
+    }
   }
 
-  return leftBracket + translated;
+  return leftBracket + translated + rightBracket;
 }
 
 function getRightBracket(text: string) {
